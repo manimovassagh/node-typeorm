a9cfc764a87ac9229be45ce762e251c3f29e7c06
Mani Movassagh <mani.mgh@gmail.com>
2024-10-03 23:46:51 +0200

    define extern routes

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,19 @@
 import dotenv from 'dotenv';
-import express, { Request, Response } from 'express';
+import express from 'express';
 import 'reflect-metadata';
-import { Student } from './entity/Student';
-import { AppDataSource } from './initDB/dbConfig';
 import { initDB } from './initDB/initDB';
-import { formatSuccess } from './utils/formatter';
+import { homeRouteInit } from './router/homeRoute';
 
-// Load environment variables from .env file
 dotenv.config();
 
-// Initialize the database
 initDB();
 
 const app = express();
 
-// Get the port from environment variables or default to 300 if not in production
+
+homeRouteInit(app);
+
 const port = process.env.PORT || 3000;
-
-// Basic express route
-app.get('/', async (_req: Request, res: Response) => {
-  const students = await AppDataSource.manager.find(Student, { relations: ['courseAttend'] });
-
-  formatSuccess(res, students);
-});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
